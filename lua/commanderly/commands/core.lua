1f546640167beccefdf66a938fe2efb1bd161713
jvs <vonseg@protonmail.com>
2022-12-30 10:02:00 -0600

    Try combining some modules.

--- lua/commanderly/commands/core.lua
+++ lua/commanderly/commands/core.lua
@@ -1,4 +1,19 @@
 local commands = {
+  -- Buffers --
+  {
+    title = "Show Next Buffer",
+    desc = "Show the next buffer in the current view.",
+    mode = "n",
+    alias = "bnext",
+  },
+  {
+    title = "Show Previous Buffer",
+    desc = "Show the previous buffer in the current view.",
+    mode = "n",
+    alias = "bprevious",
+  },
+
+  -- Paths --
   {
     title = "Copy Absolute Path",
     desc = "Copy the absolute path of the current file to the clipboard.",
@@ -37,6 +52,50 @@ local commands = {
       vim.cmd('let @" = expand("%")')
       print('Yanked "' .. vim.fn.expand("%") .. '"')
     end,
+  },
+
+  -- Splits --
+  {
+    title = "Horizontal Split",
+    desc = "Create a new horizontal split.",
+    mode = "n",
+    alias = "sp",
+  },
+  {
+    title = "Vertical Split",
+    desc = "Create a new vertical split.",
+    mode = "n",
+    alias = "vsp",
+  },
+  {
+    title = "Close Split",
+    desc = "Close the current split.",
+    mode = "n",
+    alias = "q",
+  },
+  {
+    title = "Move to Left Split",
+    desc = "Move the cursor to the left split.",
+    mode = "n",
+    run = "<C-W><C-H>",
+  },
+  {
+    title = "Move to Lower Split",
+    desc = "Move the cursor to the lower split.",
+    mode = "n",
+    run = "<C-W><C-J>",
+  },
+  {
+    title = "Move to Upper Split",
+    desc = "Move the cursor to the upper split.",
+    mode = "n",
+    run = "<C-W><C-K>",
+  },
+  {
+    title = "Move to Right Split",
+    desc = "Move the cursor to the right split.",
+    mode = "n",
+    run = "<C-W><C-L>",
   },
 }
 
