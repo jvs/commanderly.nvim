1b93d8d788b92c69552fe11208d5f9ebcfd8d1b6
jvs <vonseg@protonmail.com>
2023-01-05 20:01:01 -0600

    Add command to remove trailing whitespace.

--- lua/commanderly/commands/core.lua
+++ lua/commanderly/commands/core.lua
@@ -493,6 +493,19 @@ local commands = {
     run = "retab",
     -- TODO: Maybe have a prompt that asks for the value of tabstop.
   },
+
+  -- Text --
+  {
+    title = "Remove Trailing Whitespace",
+    id = "remove_trailing_whitespace",
+    desc = "Delete trailing whitespace in the current file.",
+    run = function()
+      local curpos = vim.api.nvim_win_get_cursor(0)
+      vim.cmd([[keeppatterns %s/\s\+$//e]])
+      vim.api.nvim_win_set_cursor(0, curpos)
+    end,
+    keywords = "delete",
+  },
 }
 
 return commands
